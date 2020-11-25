4904f6e7d1f838c3fe94452992eafd137c8ec7b0
smamarketing <ryan@smamarketing.net>
2020-11-25 12:04:22 -0500

    Create JSON-LD-WRAP.js

---
+++ Google-Tag-Manager/JSON-LD-WRAP.js
@@ -0,0 +1,16 @@
+<script>
+(function(){
+	var jsondata = {
+
+***Add JSON Here***
+
+
+};
+
+var el = document.createElement ('script');
+el.type = 'application/ld+json';
+el.innerHTML = JSON.stringify(jsondata);
+document.head.appendChild(el);
+}) ();
+
+</script>
